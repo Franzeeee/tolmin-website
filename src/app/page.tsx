c4887b30cb5b73b5113911709a64931edca31077
Franze <diazfranzpeter@gmail.com>
2025-05-28 12:06:37 +0800

    fix: adjust gradient background in .bg-red-fade class for improved visibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
       <header className="w-full h-screen grid grid-rows-[auto_1fr] bg-white landing-header">
         <MainNav />
         <div className="flex items-center justify-center">
-          <h1 className="text-8xl font-bold">NK TOLMIN</h1>
+          <h1 className="text-9xl font-bold text-gray-50 opacity-60 header-text">NK TOLMIN</h1>
         </div>
       </header>
       <h1 className="text-4xl font-bold mb-4 text-black">Welcome to My App</h1>
