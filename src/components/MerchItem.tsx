90d62aee47fec00c33584e298468917f58733cc7
Franze <diazfranzpeter@gmail.com>
2025-06-05 10:07:48 +0800

    feat: add MerchItem component and integrate it into the main page layout fix: update active index color in PlayerCarousel dots for better visibility

--- src/components/MerchItem.tsx
+++ src/components/MerchItem.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
-const Carousel: React.FC = () => {
+const MerchItem: React.FC = () => {
   const trackRef = useRef<HTMLDivElement>(null);
   const [activeIndex, setActiveIndex] = useState(0);
 
@@ -71,24 +71,26 @@ const Carousel: React.FC = () => {
         {cards.map((label, i) => (
           <div className="card text-black" key={i}>
             <motion.div
-              className='relative p-5 h-full w-full'
+              className='relative h-full w-full border-2 border-gray-200'
               initial={{ opacity: 0 }}
               whileInView={{ opacity: 1 }}
               transition={{ duration: 1, ease: "easeOut", delay: i * 0.1 }}
               viewport={{ once: true, amount: .3 }}
             >
-              <h1 className='absolute top-2 right-3 text-white z-2 text-4xl font-bold poppins uppercase player-number'>
-                01
-              </h1>
               <Image
                 src='/player1.png'
                 alt="News Image"
-                fill
                 className='object-cover'
+                height={300}
+                width={300}
               />
-              <div className='absolute w-full px-4 left-0 pb-5 bottom-0 bg-black/50 flex flex-col justify-end text-white p-4 bottom-red-gradient h-50 poppins'>
-                <p className='-mb-3 uppercase'>Altin</p>
-                <p className='text-4xl font-semibold poppins uppercase'>Manxhuka</p>
+                <div className="p-5">
+                  <a href="#">
+                      <h5 className="mb-2 text-lg text-center tracking-tight text-gray-700">Dres NK TKK Tolmin</h5>
+                  </a>
+                  <a href="#" className="inline-flex justify-center items-center w-full px-3 py-2 text-lg uppercase font-medium text-center text-white bg-gray-900 hover:bg-gray-950 focus:ring-1 focus:outline-none focus:ring-gray-500">
+                      Buy Now
+                  </a>
               </div>
             </motion.div>
           </div>
@@ -118,4 +120,4 @@ const Carousel: React.FC = () => {
   );
 };
 
-export default Carousel;
+export default MerchItem;
