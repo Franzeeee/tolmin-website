ce06d9f44ac57961b41e05d53164c8bc4e6663c0
Franze <diazfranzpeter@gmail.com>
2025-06-12 11:31:44 +0800

    fix: simplify QueryClient initialization in RootLayout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
 
-  const [queryClient] = useState(() => new QueryClient());
+  const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
